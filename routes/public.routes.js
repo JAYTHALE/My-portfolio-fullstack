ece12234d052f8cb6fab0c3da74177272a8b5046
jaythale <jaythale@123.com>
2024-10-04 13:31:49 +0530

    intial setup

--- routes/public.routes.js
+++ routes/public.routes.js
@@ -1,9 +1,10 @@
 const router = require("express").Router()
+const { rateLimit } = require("express-rate-limit")
 const publicController = require("../controller/public.controller")
 
 router
     .get("/fetch-project", publicController.fetchProjects)
-    .get("/fetch-caro", publicController.getAllCarousel)
+    .get("/fetch-caro", rateLimit({ windowMs: 15 * 60 * 1000, limit: 1 }), publicController.getAllCarousel)
     .get("/get-project-details/:id", publicController.getProjectDetails)
 
     .post("/add-contact", publicController.addContact)
